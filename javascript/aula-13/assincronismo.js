dbc1b136e15091eaadd17d83e20452a0261e0bf2
flaviare1s <flaviarster@gmail.com>
2025-01-23 15:21:50 -0300

    Aula sobre funções callback

---
+++ javascript/aula-13/assincronismo.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+
+// * 1. Callbacks
+
+// Essa é uma função assíncrona, pois ler arquivos demora um tempo.
+
+console.log('Antes de ler o arquivo...');
+
+fs.readFile('javascript/aula-13/arquivo.txt', (erro, conteudo) => {
+  if (erro) {
+    console.log(erro);
+  } else {
+    console.log(String(conteudo));
+  }
+});
+
+console.log('Depois de ler o arquivo...');
+
+console.clear();
+
+// Set Timeout
+
+setTimeout(() => {
+  console.log('Depois de 2 segundos...');
+}, 2000)
+
+console.clear();
+
+// ---------------------------------------------------------------
+
+// * 2. Promises
+
+
