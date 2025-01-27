c5705d17a5f5ca5df54cc37a407b35719d878c38
flaviare1s <flaviarster@gmail.com>
2025-01-27 11:51:33 -0300

    Exercício usando a ViaCEP API

--- javascript/aula-14/index.js
+++ javascript/aula-14/index.js
@@ -9,3 +9,19 @@ fetch('https://viacep.com.br/ws/01001000/json/').then((response) => {
 }).catch((err) => {
   console.log(err)
 })
+
+// Utilizando async/await:
+
+async function buscaCEP() {
+  try {
+    const response = await fetch(`https://viacep.com.br/ws/01001000/json/`)
+  const data = await response.json()
+  console.log(data)
+  } catch (err) {
+    console.log(err)
+  } finally {
+    console.log('Terminou a requisição!')
+  }
+}
+
+buscaCEP('01001000')
